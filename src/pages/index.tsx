f2e2f737ca82ae53c39af902d8ae5acc4a6e9fee
Piotr Woźniak <piotrw84@gmail.com>
2024-10-07 22:24:06 +0200

    deploymet test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>kotowska-portfolio</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
