9a56ee1413a0a586d17789ba8ab311be418f36f6
Piotr Woźniak <piotrw84@gmail.com>
2024-10-08 22:04:54 +0200

    updated logo component

--- src/components/LogoComponent.tsx
+++ src/components/LogoComponent.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Logo from "@/../public/svgs/logo.svg";
+import Logo from "../../public/svgs/logo.svg";
 
 const LogoComponent: React.FC = () => {
-  return <Logo></Logo>
+  return <Logo></Logo>;
 };
 
 export default LogoComponent;
