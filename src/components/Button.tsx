84d09d1734e09c1b35d0b32729d8363551cae75c
Piotr Woźniak <piotrw84@gmail.com>
2024-10-08 22:01:20 +0200

    Updated button styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import ButtonStyle from './ButtonStyle.module.css'
+import React from "react";
+import ButtonStyle from "./ButtonStyle.module.css";
+import classNames from "classnames";
 
 interface ButtonProps {
-    onClick: () => void;
-    label: string;
+  onClick: () => void;
+  label: string;
+  solid?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, label }) => {
-    return (
-        <button className={ButtonStyle.button} onClick={onClick}>
-            {label}
-        </button>
-    );
+const Button: React.FC<ButtonProps> = ({ onClick, label, solid }) => {
+  return (
+    <button
+      className={classNames(ButtonStyle.button, {
+        [ButtonStyle.solid]: solid,
+        button: true,
+      })}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
 };
-
-
 
 export default Button;
