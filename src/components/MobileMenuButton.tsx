8344149da89a28e9e97f70174994d641a2e50c8f
Piotr Woźniak <piotrw84@gmail.com>
2024-10-11 07:21:12 +0200

    Add new SVG icons drawer menu

--- src/components/MobileMenuButton.tsx
+++ src/components/MobileMenuButton.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import HamburgerMenu from "/public/svgs/hamburger-menu.svg";
 
 interface MobileMenuButtonProps {
   onClick: React.MouseEventHandler<HTMLButtonElement>;
@@ -7,21 +8,7 @@ interface MobileMenuButtonProps {
 function MobileMenuButton({ onClick }: MobileMenuButtonProps) {
   return (
     <button onClick={onClick}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-      >
-        <path
-          d="M4 12H20M4 6H20M4 18H20"
-          stroke="#1C1C1C"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
+      <HamburgerMenu className="w-6 h-6 relative" />
     </button>
   );
 }
