1f6c91817485fb86e1dc0bb90583e1d05cf5363e
Piotr Woźniak <piotrw84@gmail.com>
2024-10-12 10:13:26 +0200

    fixing scroll lock for safari

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,39 +4,58 @@ import DesktopNavLinks from "./DesktopNavLinks";
 import MobileMenuButton from "./MobileMenuButton";
 import MobileDrawer from "./MobileDrawer";
 import classNames from "classnames";
-import NavbarStyle from "./NavbarStyle.module.css";
+import NavbarStyle from "./NavbarStyle.module.css";0
 
 const Navbar: React.FC = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
   const handleDrawerToggle = () => {
     setIsDrawerOpen(!isDrawerOpen);
   };
 
-  useEffect(() => {
-    if (isDrawerOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
+  const syncHeight = () => {
+    document.documentElement.style.setProperty(
+      "--window-inner-height",
+      `${window.innerHeight}px`
+    );
+  };
 
-    return () => {
-      document.body.style.overflow = "auto";
+  const debounce = (func: (...args: any[]) => void, wait: number) => {
+    let timeout: NodeJS.Timeout;
+    return (...args: any[]) => {
+      clearTimeout(timeout);
+
+      timeout = setTimeout(() => func.apply(this, args), wait);
     };
-  }, [isDrawerOpen]);
-  
+  };
 
   useEffect(() => {
-    const handleResize = () => {
+    const handleResize = debounce(() => {
+      syncHeight();
       if (window.innerWidth >= 1024) {
         setIsDrawerOpen(false);
       }
-    };
+    }, 100);
 
+    // Initial sync
+    syncHeight();
+
+    // Add event listeners
     window.addEventListener("resize", handleResize);
+
+    // Lock body scroll when drawer is open
+    if (isDrawerOpen) {
+      document.documentElement.classList.add("is-locked");
+    } else {
+      document.documentElement.classList.remove("is-locked");
+    }
+
+    // Cleanup function
     return () => {
       window.removeEventListener("resize", handleResize);
+      document.documentElement.classList.remove("is-locked");
     };
-  }, []);
+  }, [isDrawerOpen]);
 
   return (
     <div
