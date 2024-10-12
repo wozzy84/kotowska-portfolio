a03db20ba88c17ac54e012508f403d44e9e677e7
Piotr Woźniak <piotrw84@gmail.com>
2024-10-12 20:55:37 +0200

    navbar:support dark mode

--- src/components/NavLogo.tsx
+++ src/components/NavLogo.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Logo from "/public/Logo.svg";
+import classNames from "classnames";
+import NavbarStyle from "./NavbarStyle.module.css";
 
 function NavLogo() {
   return (
-    <div className="flex justify-center items-center w-full h-full lg:justify-start">
-      <Logo />
+    <div
+      className={
+        "flex justify-center items-center w-full h-full lg:justify-start"
+      }
+    >
+      <Logo style={{ color: "var(--text-primary)" }} />
     </div>
   );
 }
