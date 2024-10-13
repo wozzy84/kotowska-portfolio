24cf2463066d656b4461f4484d2f8e6bf04b6075
Piotr Woźniak <piotrw84@gmail.com>
2024-10-13 11:04:50 +0200

    Refactor HeroSectionStyle.module.css and index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
-import Image from "next/image";
-import localFont from "next/font/local";
-import Button from "@/components/Button";
+// import localFont from "next/font/local";
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export default function Home() {
   return (
